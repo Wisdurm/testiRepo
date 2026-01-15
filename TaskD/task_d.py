749fb029f4d8e82aabff740e5a5f0c2b1ef91fa8
Wisdurm <luukas.kola@gmail.com>
2026-01-15 10:28:19 +0200

    gejriogjeiogjeriogjoer

---
+++ TaskD/task_d.py
@@ -0,0 +1,89 @@
+# Copyright (c) 2025 Luukas Kola and Luka Hietala
+# License: MIT
+import types
+import importlib
+from datetime import datetime
+import csv
+
+def read_data(filename: str) -> (list[str], list[list[str]]):
+    """
+    Reads the CSV file and returns the rows in a suitable structure.
+
+    :filename: Name of file
+    :returns:
+        :fields: Column headers in CSV
+        :rows: Rows in CSV
+    """
+
+    # Column titles
+    fields : list [str] = []
+    # Row data
+    rows : list[list[str]] = []
+    
+    with open(filename, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        fields = next(reader)
+        for row in reader:
+            rows.append(row)
+
+    return fields, rows
+
+def format_data(rows: list[str]) -> dict[datetime.date, dict[str, float]]:
+    """
+    Does calculations and formats csv data into dictionary
+    
+    :rows: List of data produced by read_data
+    :returns: Dictionary which maps days to dictionaries, which map Consumption
+    and Production into float values.
+    """
+    results : dict[datetime.date, dict[str, float]] = {}
+    for row in rows:
+        date : datetime.date = datetime.strptime(row[0], "%Y-%m-%dT%H:%M:%S").date()
+        if not date in results:
+            results[date] = {}
+            results[date]['C1'] = float(row[1]) / 1000
+            results[date]['C2'] = float(row[2]) / 1000
+            results[date]['C3'] = float(row[3]) / 1000
+            results[date]['P1'] = float(row[4]) / 1000
+            results[date]['P2'] = float(row[5]) / 1000
+            results[date]['P3'] = float(row[6]) / 1000
+        else:
+            results[date]['C1'] += float(row[1]) / 1000
+            results[date]['C2'] += float(row[2]) / 1000
+            results[date]['C3'] += float(row[3]) / 1000
+            results[date]['P1'] += float(row[4]) / 1000
+            results[date]['P2'] += float(row[5]) / 1000
+            results[date]['P3'] += float(row[6]) / 1000
+    return results
+
+def print_data(titles : list[str], data : dict[datetime.date, dict[str, float]]) -> None:
+    """
+    Prints formatted data in a pretty little table
+
+    :titles: The titles for each column
+    :data: Formatted data returned by format_data
+    :returns: Nothing
+    """
+    # Print titles
+    header = "\t".join(titles)
+    print(header)
+    # Print data
+    paivat : list[str] = ["Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai", "Sunnuntai"]
+    for day, data in data.items():
+        paiva : str = paivat[day.weekday()]
+        date_str : str = day.strftime("%d.%m.%Y")
+        print(f'{paiva} \t{date_str} {f'\t{data['C1']:.2f}\t{data['C2']:.2f}\t{data['C3']:.2f}\t{data['P1']:.2f}\t{data['P2']:.2f}\t{data['P3']:.2f}'.replace('.',',')}')
+
+def main() -> None:
+    """
+    Main function: reads data, computes daily totals, and prints the report.
+    """
+
+    fields, rows = read_data('week42.csv')
+    # Convert CSV data into desired format
+    formatted_data = format_data(rows)
+    # Print formatted data as table
+    print_data(fields, formatted_data)
+
+if __name__ == "__main__":
+    main()
